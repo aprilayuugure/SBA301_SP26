eab589c29e28c735ab1a7a0f5aa8ffc29db0e1c1
aprilayuugure <minhquaningenious@gmail.com>
2026-01-08 13:24:38 +0700

    Add LAB folder

---
+++ LAB/LAB1/src/components/Orchid.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Button, Card} from 'react-bootstrap';
+
+function Orchid ( {orchid} ) {
+    const { id, name, image, description, category, isSpecial } = orchid;
+    
+    return (
+    <Card className="p-3 mx-auto">
+        {   
+            isSpecial && (
+                <span
+                    className="badge bg-success position-absolute" style={{ top: "10px", left: "10px", zIndex: 1 }}
+                >
+                    Special
+                </span>
+            )
+        }    
+
+        <Card.Img variant = "top" style = {{ width: '200px', height: '200px', borderRadius: '0px', objectFit: 'cover', margin: 'auto' }} src = {image} />
+        <Card.Body>
+            <Card.Title className = "text-center">{name}</Card.Title>
+            <Card.Text>Category: {category}</Card.Text>
+        </Card.Body>      
+
+        <Button variant = "primary" className = "w-50 mx-auto">Details</Button>
+    </Card> 
+    )
+}
+
+export default Orchid;
