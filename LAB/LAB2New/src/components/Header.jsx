3b814489b7c4c67cd9b949c85870c46a43dd1034
aprilayuugure <minhquaningenious@gmail.com>
2026-01-20 10:25:02 +0700

    Add LAB2New folder

---
+++ LAB/LAB2New/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import { Container, Button } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Searchbar from './Searchbar';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+function Header( { state, dispatch, onSearchChange }) {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch({ type: 'LOGOUT' });
+    navigate(`/login`);
+  }
+
+  return (
+    <Navbar bg = "light" sticky = "top">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Orchid Store</Navbar.Brand>
+
+        <Nav className="me-auto">
+          <Nav.Link as = {Link} to="/orchids">Home</Nav.Link>
+          <Nav.Link as = {Link} to="/about">About</Nav.Link>
+          <Nav.Link as = {Link} to="/contact">Contact</Nav.Link>
+        </Nav>
+
+        <div>
+            <Searchbar onSearchChange = {onSearchChange} />
+        </div>
+
+        {!state.isAuthenticated ? (
+          <Button variant = "primary" className = "mx-3" onClick = {() => navigate(`/login`)}>Login</Button>
+        ) : (
+          <Button variant = "secondary" className = "mx-3" onClick = {handleLogout}>Logout</Button>
+          )
+        }
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
